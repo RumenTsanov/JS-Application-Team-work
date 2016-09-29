e5a26bdfcd3cdbb566c73e2c1688595f8feaacb4
RumenTsanov <rumentsanov88@gmail.com>
2016-09-29 15:53:44 +0300

    Login and register buttons logic!

--- VideoForum/public/scripts/main.js
+++ VideoForum/public/scripts/main.js
@@ -8,3 +8,41 @@ router
     .on('', homeController.all)
     .on('/posts', postsController.all)
     .resolve();
+
+//Log in events and requests logic!
+
+//Login button
+$('#btn-login').on('click', function() {
+    var user = {
+        username: $('#tb-username').val(),
+        passHash: $('#tb-password').val()
+    };
+
+    $.ajax({
+        url: '',
+        method: 'PUT',
+        data: JSON.stringify(user),
+        contentType: 'application/json',
+        success: function(user) {
+            this.redirect('#/');
+        }
+    });
+});
+
+//Register button
+$('#btn-register').on('click', function() {
+    var user = {
+        username: $('#tb-username').val(),
+        passHash: $('#tb-password').val()
+    };
+
+    $.ajax({
+        url: '',
+        method: 'POST',
+        data: JSON.stringify(user),
+        contentType: 'application/json',
+        success: function(user) {
+            this.redirect('#/');
+        }
+    });
+});
