12d2ffd390f8b54779b1069e5828e8b5479e79e5
RumenTsanov <rumentsanov88@gmail.com>
2016-10-01 17:40:54 +0300

    Added users-controller.js! NOT ready log in function!

---
+++ VideoForum/public/controllers/users-controller.js
@@ -0,0 +1,50 @@
+import { templates } from '../scripts/templates.js';
+import { data } from '../scripts/data.js';
+
+let usersContoller = function() {
+
+    function register(context) {
+        templates.get('register')
+            .then(function(template) {
+                context.$element().html(template());
+
+                $('#btn-register').on('click', function() {
+                    var user = {
+                        username: $('#tb-username').val(),
+                        password: $('#tb-password').val(),
+                        email: $('#tb-email').val(),
+                        displayName: $('#tb-displayName').val()
+                    };
+
+                    data.users.register(user)
+                        .then(function() {
+                            context.redirect('#/');
+                            document.location.reload(true);
+                        });
+                });
+            });
+    }
+
+    function login(context) {
+        $.('#btn-login').on('click', function() {
+            var user = {
+                username: $('#tb-username').val(),
+                password: $('#tb-password').val(),
+            };
+
+            data.users.login(user)
+                .then(function() {
+                    context.redirect('#/');
+                    document.location.reload(true);
+                });
+        });
+
+    }
+
+    return {
+        register: register,
+        login: login
+    };
+}();
+
+export { usersController };
